f3554eb4d3eba80e828c0a872fbd7f9a3899e471
Santosh Mane <premmane3092001@gmail.com>
2023-10-01 03:18:43 +0530

    backend connected to Atlas

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-MONGO_URI = "mongodb://127.0.0.1/inotebook";
+const dotenv = require('dotenv');
+dotenv.config({path:__dirname+'/config.env'});
+//MONGO_URI = "mongodb://127.0.0.1/inotebook";
+const MONGO_URI = process.env.DB;
 const connectToMongo = async () => {
     try {
       let result = await mongoose.connect(MONGO_URI);
